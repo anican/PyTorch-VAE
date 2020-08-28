d9ef978992fcc4dcce9fc65cf8b57ad64198dc18
Ani Canumalla <anirudhc@cs.washington.edu>
2020-08-28 03:32:56 -0700

    Update run.py

--- run.py
+++ run.py
@@ -41,15 +41,10 @@ model = vae_models[config['model_params']['name']](**config['model_params'])
 experiment = VAEXperiment(model,
                           config['exp_params'])
 
-runner = Trainer(default_save_path=f"{tt_logger.save_dir}",
-                 min_nb_epochs=1,
-                 logger=tt_logger,
-                 log_save_interval=100,
-                 train_percent_check=1.,
-                 val_percent_check=1.,
-                 num_sanity_val_steps=5,
-                 early_stop_callback = False,
-                 **config['trainer_params'])
+runner = Trainer(default_root_dir=f"{tt_logger.save_dir}",
+                     logger=tt_logger,
+                     num_sanity_val_steps=5,
+                     **config['trainer_params'])
 
 print(f"======= Training {config['model_params']['name']} =======")
 runner.fit(experiment)
